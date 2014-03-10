f491ff2f04c5375b460ba9aefa0fb1f2c64ef3a5
csvenja <c.svenjax@gmail.com>
2014-03-10 00:24:06 -0700

    Initial Commit

---
+++ Robot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Robot
+//
+//  Created by Svenja on 2014-03-10.
+//  Copyright (c) 2014 Shan Cao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
